2192dfc7bd7d5cb6a8d4af536908062e282ecc2d
mora5925 <wahba190@gmail.com>
2020-11-18 09:42:01 -0500

    adding search box and search button

--- nutritionmatters/src/App.js
+++ nutritionmatters/src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
      <h1>Nutrition Matters</h1>
+     <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}  />
+        <button className="search-button" type="submit">search</button>
+      </form>
     </div>
   );
 }
