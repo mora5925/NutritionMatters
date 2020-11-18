586a2161b1210edd767bf781b7eaede290825626
mora5925 <wahba190@gmail.com>
2020-11-18 10:00:27 -0500

    adding api const

--- nutritionmatters/src/components/Recipe.js
+++ nutritionmatters/src/components/Recipe.js
@@ -1,14 +1,8 @@
 import React from 'react'
 
- const Recipe = ({label, image, url, ingredients}) => {
-  return (
+ 
     <div>
-      <h2>{label}</h2>
-      <img src={image} alt={label}/>
-      <a href={url} target="_blank"
-      rel="noopener noreferrer">
-        URL
-      </a>
+     
       <button>ingredients</button>
 
       
